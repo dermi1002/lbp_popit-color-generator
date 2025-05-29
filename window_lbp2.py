d65b901b64a8c5cef7c27e84af8182bd6d1167f9
turnernator <dermi1002@protonmail.com>
2025-05-28 21:21:59 -0700

    Continued progress in making code readable

--- window_lbp2.py
+++ window_lbp2.py
@@ -26,6 +26,9 @@ class ColorTabList(ctk.CTkTabview):
                     self.Green = int(self.green_slider.get())
                     self.Blue = int(self.blue_slider.get())
 
+                    # color slider debugging
+                    # print(f'Current Color: {self.Red}, {self.Green}, {self.Blue}')
+
                     self._hexprint = '%02X%02X%02X' % (self.Red, self.Green, self.Blue)
 
                     self.color_preview.configure(bg_color = f'#{self._hexprint}')
@@ -36,52 +39,50 @@ class ColorTabList(ctk.CTkTabview):
                 def copy_hexentry():
                     pyperclip.copy(self._hexentry.get())
 
-                color_beginning_value:str = '000000'
+                color_beginning_value: str = '000000'
 
                 self.color_preview = ctk.CTkLabel(
                     master, 
                     text = '', 
                     bg_color = f'#{color_beginning_value}', 
-                    width = 200, 
-                    height = 200
+                    width = 200, height = 200
                     )
 
                 self.color_preview.place(y = 4)
 
-                # am i coupling code at this point
-                class RGBSlider(ctk.CTkSlider): # Unused
-                    def __init__(self, master, slider_y_position, *args, **kwargs):
-                        super().__init__(master, slider_y_position, *args, **kwargs)
+                # class RGBSlider(ctk.CTkSlider): # Unused
+                    # def __init__(self, master, slider_y_position, *args, **kwargs):
+                        # super().__init__(master, slider_y_position, *args, **kwargs)
 
-                        slider_x_position:int = 260
-                        slider_y_position:int = slider_y_position
+                        # slider_x_position: int = 260
+                        # slider_y_position: int = slider_y_position
                         
-                        self = ctk.CTkSlider(
-                            master, 
-                            from_ = 0, to = 255, 
-                            width = 256, 
-                            command = change_color
-                            )
+                        # self = ctk.CTkSlider(
+                            # master, 
+                            # from_ = 0, to = 255, # why does it output 0.5
+                            # width = 256, 
+                            # command = change_color
+                            # )
 
-                        self.configure(number_of_steps = 256)
-                        self.set(0)
-                        self.place(x = slider_x_position, y = slider_y_position)
+                        # self.configure(number_of_steps = 256)
+                        # self.set(0)
+                        # self.place(x = slider_x_position, y = slider_y_position)
 
-                slider_value_range:int = 256
-                slider_x_position:int = 260    
-                rgb_letter_x_position:int = 225
-                hex_related_y_position:int = 170
+                slider_max_value:int = 255
+                slider_value_range: int = 256
+                slider_x_position: int = 260    
+                rgb_letter_x_position: int = 225
+                hex_related_y_position: int = 170
 
-                # i really wish i could get the slider class to work but i can't use the command therefrom
+                # i really wish i could get the slider class to work but i can't get the max value to work
                 self.letter_r = ctk.CTkLabel(master, text = 'R').place(x = rgb_letter_x_position, y = 10)
-                self.red_slider = ctk.CTkSlider(master, from_ = 0, to = 255, width = 256, command = change_color)
-
+                self.red_slider = ctk.CTkSlider(master, from_ = 0, to = slider_max_value, width = slider_value_range, command = change_color)
+                
                 self.letter_g = ctk.CTkLabel(master, text = 'G').place(x = rgb_letter_x_position, y = 60)
-                self.green_slider = ctk.CTkSlider(master, from_ = 0, to = 255, width = 256, command = change_color)
+                self.green_slider = ctk.CTkSlider(master, from_ = 0, to = slider_max_value, width = slider_value_range, command = change_color)
 
                 self.letter_b = ctk.CTkLabel(master, text = 'B').place( x = rgb_letter_x_position, y = 110)
-                self.blue_slider = ctk.CTkSlider(master, from_ = 0, to = 255, width = 256, command = change_color)
-
+                self.blue_slider = ctk.CTkSlider(master, from_ = 0, to = slider_max_value, width = slider_value_range, command = change_color)
 
                 self.red_slider.configure(number_of_steps = slider_value_range)
                 self.red_slider.set(0)
@@ -147,9 +148,9 @@ class ColorTabList(ctk.CTkTabview):
             yaml_set = ctk.filedialog.askopenfilename(title = "Import YAML Config", initialdir = "./save")
             if yaml_set is None:
                 return
-            with open("color_path.yaml", "w") as fo:
-                fo.truncate(0)
-                fo.write(
+            with open("color_path.yaml", "w") as color_path_file:
+                color_path_file.truncate(0)
+                color_path_file.write(
                     "color-path:\n" + 
                     "  set-yaml: " + "\"" + str(yaml_set) + "\"" # path to recently saved color yaml file
                 )
@@ -165,14 +166,14 @@ class ColorTabList(ctk.CTkTabview):
             with open(current_color['set-yaml'], 'r') as file:
                 config = yaml.safe_load(file)
 
-            db_config = config['color-code']
+            yaml_config = config['color-code']
 
 
-            player_id:str = "00DC5E8C"
+            player_id: str = "00DC5E8C"
 
             profile_id_1 = ["00000000", "00000004", "00000008", "0000000C"]
 
-            netcheat_zeroes:str = "0 00000000 "
+            netcheat_zeroes: str = "0 00000000 "
 
             ncl_save = ctk.filedialog.asksaveasfile(
                 title = "Export NCL Code", 
@@ -185,40 +186,41 @@ class ColorTabList(ctk.CTkTabview):
                 return
 
             color_code = str(
-                db_config['caption'] + '\n' + '0\n' + 
+                yaml_config['caption'] + '\n' + '0\n' + 
 
+                # i couldn't put yaml variables inside formatted strings so
                 f'6 {player_id} {profile_id_1[0]}\n' + 
-                netcheat_zeroes + db_config['primopacity'] + db_config['primcolor'] + '\n' + 
+                netcheat_zeroes + yaml_config['primopacity'] + yaml_config['primcolor'] + '\n' + 
 
                 f'6 {player_id} {profile_id_1[1]}\n' + 
-                netcheat_zeroes + db_config['secopacity'] + db_config['seccolor']+ '\n' + 
+                netcheat_zeroes + yaml_config['secopacity'] + yaml_config['seccolor']+ '\n' + 
 
                 f'6 {player_id} {profile_id_1[2]}\n' + 
-                netcheat_zeroes + db_config['tertopacity'] + db_config['tertcolor'] + '\n' + 
+                netcheat_zeroes + yaml_config['tertopacity'] + yaml_config['tertcolor'] + '\n' + 
                 
                 f'6 {player_id} {profile_id_1[3]}\n' + 
-                netcheat_zeroes + db_config['emphopacity'] + db_config['emphcolor'] + '\n#\n'
+                netcheat_zeroes + yaml_config['emphopacity'] + yaml_config['emphcolor'] + '\n#\n'
                 )
             ncl_save.write(color_code)
             ncl_save.close()
 
         #tab here
-        expframe = ctk.CTkFrame(self.tab('Export'))
-        expframe.grid(row = 0, column = 0)
+        export_tab = ctk.CTkFrame(self.tab('Export'))
+        export_tab.grid(row = 0, column = 0)
 
-        exp_text_preview = ctk.CTkLabel(
-            expframe, 
+        export_text_preview = ctk.CTkLabel(
+            export_tab, 
             text = f'If you are finished with your Popit color theme,\ngive it a Code Name and export a cheat file!', 
             width = 525
             )
 
-        exp_text_preview.grid(row = 0, pady = 5)
+        export_text_preview.grid(row = 0, pady = 5)
 
-        code_name = ctk.CTkEntry(expframe, placeholder_text = 'Code Name')
+        code_name = ctk.CTkEntry(export_tab, placeholder_text = 'Code Name')
 
-        _savebutton = ctk.CTkButton(expframe, text = 'Save YAML Config', command = export_yaml)
+        _savebutton = ctk.CTkButton(export_tab, text = 'Save YAML Config', command = export_yaml)
 
-        _exportbutton = ctk.CTkButton(expframe, text = "Convert to NCL", command = export_ncl)
+        _exportbutton = ctk.CTkButton(export_tab, text = "Convert to NCL", command = export_ncl)
 
         code_name.grid(row = 1, pady = 10)
         _savebutton.grid(row = 2, pady = 10)
