a968383986bae23fd49f841ed3f8dab533a6beeb
turnernator <dermi1002@protonmail.com>
2025-09-11 11:32:44 -0700

    Slight tweak for Hex Entry keybinds

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ class ColorTab(ctk.CTkFrame):
         def hex_certain_characters(event):
             if event.char in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'a', 'b', 'c', 'd', 'e', 'f'):
                 return True
-            elif event.keysym not in ('Alt_r', 'Alt_L', 'F4', 'BackSpace', 'Return', 'Left', 'Right'):
+            elif event.keysym not in ('Alt_r', 'Alt_L', 'F4', 'BackSpace', 'Return', 'Left', 'Right', 'Control_L' 'V'):
                 return 'break'
             else:
                 return False
