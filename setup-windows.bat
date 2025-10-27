27f533736fd2f7d59686a370995b3c7e8105ccef
dermi1002 <49495596+dermi1002@users.noreply.github.com>
2025-10-26 19:22:55 -0700

    Batch Setup Script almost completed

--- setup-windows.bat
+++ setup-windows.bat
@@ -1,24 +1,41 @@
 @echo on
 
-:: THIS IS AN UNTESTED APPROXIMATION. USE AT YOUR OWN RISK.
-
 :: Confirmation of Internet Connection
 SETLOCAL EnableDelayedExpansion
 CHOICE /M "This script requires connection to the Internet. Continue?" 
-goto sub_%ERRORLEVEL%
+if ERRORLEVEL 1 goto autoSetup
+if ERRORLEVEL 2 goto eof
 
-:sub_2
-exit
-goto:eof
-
-:sub_1
-ECHO Creating Virtual Environment...
+:autoSetup
+echo Creating Virtual Environment...
 python -m venv .venv_windows
-ECHO Activating Virtual Environment...
-.venv_windows\Scripts\Activate
-ECHO Updating Pip, Setuptools, Wheel...
-python -m pip install --upgrade pip --upgrade setuptools --upgrade wheel
-ECHO Installing Requirements...
-pip install -r src\requirements.txt
+echo Updating Pip, Setuptools, Wheel...
+:: Calling python from the virtual environment eliminates the need to activate the virtual environment itself
+.venv_windows\Scripts\python.exe -m pip install --upgrade pip --upgrade setuptools --upgrade wheel
+echo Installing Requirements...
+.venv_windows\Scripts\python.exe -m pip install -r src\requirements.txt
+
+set PCGSCRIPT=lbp_pcg.bat
+
+if exist %PCGSCRIPT% (
+	CHOICE /M "A script named 'lbp_pcg.bat' already exists. Overwrite all data within it?"
+	if ERRORLEVEL 1 goto writeScript
+	if ERRORLEVEL 2 goto setupComplete
+) else goto:writeScript
+
+:writeScript
+echo @echo off > %PCGSCRIPT%
+echo start .venv_windows\Scripts\pythonw.exe src\main.py >> %PCGSCRIPT%
+
+:setupComplete
+CHOICE /M "Setup successfully completed! Would you like to run the Main Script?"
+if ERRORLEVEL 1 goto startMain
+if ERRORLEVEL 2 goto eof
+
+:startMain
+.venv_windows\Scripts\python.exe src\main.py
+
+:eof
+pause
 
 ENDLOCAL
