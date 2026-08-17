9934dc921027f6d2b97c5973a900a14cddf63572
dermi1002 <49495596+dermi1002@users.noreply.github.com>
2026-08-16 21:37:21 -0700

    Fixed subroutine routing errors, (attempted to) fix escape character mistake.

--- setup-windows.bat
+++ setup-windows.bat
@@ -3,8 +3,8 @@ setlocal EnableDelayedExpansion
 
 rem Confirmation of Internet Connection
 choice /m "This script requires connection to the Internet. Continue?" 
-if ERRORLEVEL 1 goto autoSetup
-if ERRORLEVEL 2 goto setupAborted
+if ERRORLEVEL 1 goto:autoSetup
+if ERRORLEVEL 2 goto:setupAborted
 
 :autoSetup
 echo Creating Virtual Environment...
@@ -18,9 +18,9 @@ set PCGSCRIPT=lbp_pcg.bat
 
 if exist %PCGSCRIPT% (
 	choice /m "A script named 'lbp_pcg.bat' already exists. Overwrite all data within it?"
-	if ERRORLEVEL 1 goto writeScript
-	if ERRORLEVEL 2 goto setupComplete
-) else goto writeScript
+	if ERRORLEVEL 1 goto:writeScript
+	if ERRORLEVEL 2 goto:setupComplete
+) else goto:writeScript
 
 :writeScript
 echo @echo off> %PCGSCRIPT%
@@ -29,10 +29,10 @@ echo.>> %PCGSCRIPT%
 echo set virtualEnvironment=.venv_windows\>> %PCGSCRIPT%
 
 echo.>> %PCGSCRIPT%
-echo if not exist %%virtualEnvironment%% goto errorMessage>> %PCGSCRIPT%
+echo if not exist %%virtualEnvironment%% goto:errorMessage>> %PCGSCRIPT%
 
 echo.>> %PCGSCRIPT%
-echo goto :startMainScript>> %PCGSCRIPT%
+echo goto:startMainScript>> %PCGSCRIPT%
 
 echo.>> %PCGSCRIPT%
 echo :errorMessage>> %PCGSCRIPT%
@@ -55,16 +55,16 @@ echo start .venv_windows\Scripts\pythonw.exe src\main.py>> %PCGSCRIPT%
 rem It's ugly, I know...
 
 :setupComplete
-choice /m "Setup successfully completed! Would you like to run the Main Script?"
-if ERRORLEVEL 2 goto endOfFile
-if ERRORLEVEL 1 goto startMain
+choice /m "Setup successfully completed^! Would you like to run the Main Script?"
+if ERRORLEVEL 2 goto:endOfFile
+if ERRORLEVEL 1 goto:startMain
 
 :startMain
 .venv_windows\Scripts\python.exe src\main.py
 
 :setupAborted
 echo Setup Aborted
-goto endOfFile
+goto:endOfFile
 
 :endOfFile
 pause
