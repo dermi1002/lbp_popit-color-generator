c52921f38fe1b4bbaecd0fbba8cb1cb91bd01c36
dermi1002 <49495596+dermi1002@users.noreply.github.com>
2026-08-17 10:06:43 -0700

    oops forgot the setup script change

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -6,6 +6,7 @@ text_cyan=$(tput setaf 6)
 text_yellow=$(tput setaf 3)
 text_reset=$(tput sgr0) # resets all text style attributes
 
+# Styled Phrases
 lbp_pcg_prefix="${text_bold}${text_cyan}LBP Popit Color Generator${text_reset}:"
 lbp_pcg_script="${text_bold}${text_yellow}lbp_pcg.sh${text_reset}"
 
@@ -14,6 +15,18 @@ read -p "$lbp_pcg_prefix This script requires connection to the Internet. Contin
 [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
 trap 'echo "$lbp_pcg_prefix Has an error occurred?"' ERR
+
+VIRTUALENV=.venv_ubuntu/
+
+# if there's a directory with the same name as '.venv_ubuntu'...
+if [ -d "$VIRTUALENV" ]; then
+	read -p "$lbp_pcg_prefix A directory named '.venv_ubuntu/' already exists. Overwrite all data within it? (Y/n): " confirm && \
+	[[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+
+	printf "$lbp_pcg_prefix Removing Virtual Environment...\n"
+	rm -rf $VIRTUALENV
+fi
+
 printf "$lbp_pcg_prefix Creating Virtual Environment...\n" && \
 python3 -m venv .venv_ubuntu && \
 printf "$lbp_pcg_prefix Updating Pip...\n" &&\
@@ -27,6 +40,9 @@ SCRIPTFILE=lbp_pcg.sh
 if [ -e $SCRIPTFILE ]; then
 	read -p "$lbp_pcg_prefix A script named 'lbp_pcg.sh' already exists. Overwrite all data within it? (Y/n): " confirm && \
 	[[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+
+	printf "$lbp_pcg_prefix Removing contents from $lbp_pcg_script...\n"
+	truncate -s 0 $SCRIPTFILE
 else
 	printf "$lbp_pcg_prefix Creating $lbp_pcg_script...\n"
 	touch lbp_pcg.sh
@@ -61,7 +77,7 @@ EndOfScript
 printf "$lbp_pcg_prefix Granting execution permissions to $lbp_pcg_script...\n"
 chmod +x lbp_pcg.sh
 
-read -p "$lbp_pcg_prefix Setup completed successfully! Would you like to run the main script? (Y/n): " confirm && \
+read -p "$lbp_pcg_prefix Setup successfully completed! Would you like to run the main script? (Y/n): " confirm && \
 [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
 printf "$lbp_pcg_prefix Starting Main Script...\n" && \
