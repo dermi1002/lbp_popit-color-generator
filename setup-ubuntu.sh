7031899d972b742ae8424bce6952d2b2a45b4072
dermi1002 <49495596+dermi1002@users.noreply.github.com>
2026-08-20 12:50:03 -0700

    Changed function and variable names to keep consistency with the rest of the project.

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Text Styling
-text_bold=$(tput bold)
-text_cyan=$(tput setaf 6)
-text_yellow=$(tput setaf 3)
-text_reset=$(tput sgr0) # resets all text style attributes
+textBold=$(tput bold)
+textCyan=$(tput setaf 6)
+textYellow=$(tput setaf 3)
+textReset=$(tput sgr0) # resets all text style attributes
 
 # Styled Phrases
-lbp_pcg_prefix="${text_bold}${text_cyan}LBP Popit Color Generator${text_reset}:"
-lbp_pcg_script="${text_bold}${text_yellow}lbp_pcg.sh${text_reset}"
+lbpPcgPrefix="${textBold}${textCyan}LBP Popit Color Generator${textReset}:"
+lbpPcgScript="${textBold}${textYellow}lbp_pcg.sh${textReset}"
 
 # File/Directory Paths
 VIRTUALENV=.venv_ubuntu/ # "Env" being short for "Environment"
@@ -18,7 +18,7 @@ SCRIPTFILE=lbp_pcg.sh
 
 # Functions
 
-function yesNoPrompt() {
+function yes_no_prompt() {
 	local promptText
 	local yesCommand
 	local noCommand
@@ -27,145 +27,145 @@ function yesNoPrompt() {
 	yesCommand="$2"
 	noCommand="$3"
 
-	read -p "$lbp_pcg_prefix $promptText (Y/n): " confirm && \
+	read -p "$lbpPcgPrefix $promptText (Y/n): " confirm && \
 	[[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || "$noCommand"
 	# I'll change this into a more readable format
 
 	"$yesCommand"
 }
 
-function removeVirtualEnv() {
-	printf "$lbp_pcg_prefix Removing Virtual Environment...\n" && \
+function remove_virtual_env() {
+	printf "$lbpPcgPrefix Removing Virtual Environment...\n" && \
 	rm -rf "$VIRTUALENV"
 }
 
-function incompleteMessage() {
+function incomplete_message() {
 	local quitMessage
 
 	quitMessage="$1"
 
-	read -p "$lbp_pcg_prefix $quitMessage Press any key to continue..." -n1 -s && \
+	read -p "$lbpPcgPrefix $quitMessage Press any key to continue..." -n1 -s && \
 	exit 1
 }
 
-function quitAborted() {
-	"incompleteMessage" "Setup Aborted."
+function quit_aborted() {
+	"incomplete_message" "Setup Aborted."
 }
 
-function quitSuccess() {
-	printf "$lbp_pcg_prefix Quitting...\n" && \
+function quit_success() {
+	printf "$lbpPcgPrefix Quitting...\n" && \
 	exit 0
 }
 
 # Sequence
 
-function internetPackageConfirm() {
-	"yesNoPrompt" "This script requires connection to the Internet and Packages for Python's Virtual Environment System and GUI Library Tkinter. Continue?" "virtualEnvironment" "quitAborted"
+function internet_package_confirm() {
+	"yes_no_prompt" "This script requires connection to the Internet and Packages for Python's Virtual Environment System and GUI Library Tkinter. Continue?" "virtual_environment_setup" "quit_aborted"
 }
 
-function virtualEnvironment() {
+function virtual_environment_setup() {
 	# if there's a directory with the same name as '.venv_ubuntu'...
 	if [ -d "$VIRTUALENV" ]; then
-		"yesNoPrompt" "A directory named '.venv_ubuntu/' already exists. Overwrite all data within it?" "removeVirtualEnv" "mainScript"
+		"yes_no_prompt" "A directory named '.venv_ubuntu/' already exists. Overwrite all data within it?" "remove_virtual_env" "main_script_setup"
 	fi
 
-	printf "$lbp_pcg_prefix Creating Virtual Environment...\n" && \
+	printf "$lbpPcgPrefix Creating Virtual Environment...\n" && \
 	python3 -m venv .venv_ubuntu && \
-	printf "$lbp_pcg_prefix Updating Pip...\n" && \
+	printf "$lbpPcgPrefix Updating Pip...\n" && \
 	.venv_ubuntu/bin/python -m pip install --upgrade pip && \
-	printf "$lbp_pcg_prefix Installing Requirements...\n" && \
+	printf "$lbpPcgPrefix Installing Requirements...\n" && \
 	.venv_ubuntu/bin/python -m pip install -r src/requirements.txt && \
 
-	"mainScript"
+	"main_script_setup"
 }
 
-function mainScript() {
+function main_script_setup() {
 	# if there's a shell script with the same name as 'lbp_pcg.sh'...
 	if [ -e "$SCRIPTFILE" ]; then
-		"yesNoPrompt" "A script named 'lbp_pcg.sh' already exists. Overwrite all data within it?" "mainScriptContent" "setupComplete"
+		"yes_no_prompt" "A script named 'lbp_pcg.sh' already exists. Overwrite all data within it?" "main_script_content" "setup_complete"
 	else
-		printf "$lbp_pcg_prefix Creating $lbp_pcg_script...\n"
+		printf "$lbpPcgPrefix Creating $lbpPcgScript...\n"
 		touch lbp_pcg.sh
-		"mainScriptContent"
+		"main_script_content"
 	fi
 }
 
-function mainScriptContent() {
-	printf "$lbp_pcg_prefix Giving contents to $lbp_pcg_script...\n" && \
+function main_script_content() {
+	printf "$lbpPcgPrefix Giving contents to $lbpPcgScript...\n" && \
 
 	# oh THANK GOODNESS! also using white spaces as tabs for the sake of this process
 	cat > "$SCRIPTFILE" <<- EndOfScript
 	#!/bin/bash
 
 	# Text styling for LBP PCG Name
-	text_bold=\$(tput bold)
-	text_cyan=\$(tput setaf 6)
-	text_reset=\$(tput sgr0) # resets all text style attributes
+	textBold=\$(tput bold)
+	textCyan=\$(tput setaf 6)
+	textReset=\$(tput sgr0) # resets all text style attributes
 
-	lbp_pcg_prefix="\${text_bold}\${text_cyan}LBP Popit Color Generator\${text_reset}:"
+	lbpPcgPrefix="\${textBold}\${textCyan}LBP Popit Color Generator\${textReset}:"
 
 	# Script's Code
 
 	# Functions
 
-	function errorMessage() {
+	function error_message() {
 	    local messageText
 
 	    messageText="\$1"
 
-	    read -p "\$lbp_pcg_prefix \$messageText Press any key to continue..." -n1 -s && \\
+	    read -p "\$lbpPcgPrefix \$messageText Press any key to continue..." -n1 -s && \\
 	    exit 1
 	}
 	
 	function run_main_program() {
-	    printf "\$lbp_pcg_prefix Starting Main Script...\n" && \\
+	    printf "\$lbpPcgPrefix Starting Main Script...\n" && \\
 	    .venv_ubuntu/bin/python src/main.py && \\
-	    printf "\$lbp_pcg_prefix Quitting...\n"
+	    printf "\$lbpPcgPrefix Quitting...\n"
 	}
 
-	function checkFiles() {
+	function check_files() {
 	    if [ ! -d .venv_ubuntu ]; then
-	        "errorMessage" "The Virtual Environment directory doesn't exist. Execute the Setup script or make the Virtual Environment yourself."
+	        "error_message" "The Virtual Environment directory doesn't exist. Execute the Setup script or make the Virtual Environment yourself."
 	    elif [ ! -e src/main.py ]; then
-	        "errorMessage" "Cannot find the Main Program in 'src/main.py'. Make sure you have left everything as it was when the project was downloaded."
+	        "error_message" "Cannot find the Main Program in 'src/main.py'. Make sure you have left everything as it was when the project was downloaded."
 	    else
 	        "run_main_program"
 	    fi
 	}
 
 	function main() {
-	    trap 'printf "\$lbp_pcg_prefix Has an error occurred?\n"' ERR
-	    "checkFiles"
+	    trap 'printf "\$lbpPcgPrefix Has an error occurred?\n"' ERR
+	    "check_files"
 	}
 
 	"main"
 	EndOfScript
 
-	printf "$lbp_pcg_prefix Granting execution permissions to $lbp_pcg_script...\n" && \
+	printf "$lbpPcgPrefix Granting execution permissions to $lbpPcgScript...\n" && \
 	chmod +x lbp_pcg.sh
 
-	"setupComplete"
+	"setup_complete"
 }
 
-function setupComplete() {
-	"yesNoPrompt" "Setup successfully completed! Would you like to run the main script?" "runMainScript" "quitSuccess"
+function setup_complete() {
+	"yes_no_prompt" "Setup successfully completed! Would you like to run the main script?" "run_main_python" "quit_success"
 }
 
-function runMainScript() {
+function run_main_python() {
 	if [ ! -e src/main.py ]; then
-		"incompleteMessage" "Cannot find the Main Program in 'src/main.py'. Make sure you have left everything as it was when the project was downloaded."
+		"incomplete_message" "Cannot find the Main Program in 'src/main.py'. Make sure you have left everything as it was when the project was downloaded."
 	else
-		printf "$lbp_pcg_prefix Starting Main Script...\n" && \
+		printf "$lbpPcgPrefix Starting Main Script...\n" && \
 		.venv_ubuntu/bin/python src/main.py && \
-		"quitSuccess"
+		"quit_success"
 	fi
 }
 
 # Execution
 
 function main() {
-	trap 'echo "$lbp_pcg_prefix Has an error occurred?"' ERR
-	"internetPackageConfirm"
+	trap 'echo "$lbpPcgPrefix Has an error occurred?"' ERR
+	"internet_package_confirm"
 }
 
 "main"
